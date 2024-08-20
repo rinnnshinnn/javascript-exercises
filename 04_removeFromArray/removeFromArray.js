1e0559058b8c31f4b3a9ec98bb271e34e2561d19
rinnnshinnn <rinnnshinnn@gmail.com>
2024-08-20 15:18:14 +0800

    Refactor removeFromArray function to handle multiple elements removal

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(arrayNum = [], removeNum, ...removeMoreNum) {
+    for (let i = arrayNum.length - 1; i >= 0; i--) {
+        if (arrayNum[i] === removeNum || removeMoreNum.includes(arrayNum[i])) {
+            arrayNum.splice(i, 1);
+        }
+    }
+
+    return arrayNum;
 };
 
 // Do not edit below this line
