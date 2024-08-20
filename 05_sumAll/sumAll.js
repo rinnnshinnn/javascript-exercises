73e08d502c05bb83a3ef1c97422ffeb30efa302f
rinnnshinnn <rinnnshinnn@gmail.com>
2024-08-20 18:34:16 +0800

    Refactor sumAll function to include input validation and range swapping logic

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(startingNum, endNum) {
+    if (!Number.isInteger(startingNum) || !Number.isInteger(endNum)) {
+        return "ERROR";
+    }
 
+    if (startingNum < 0 || endNum < 0) {
+        return "ERROR";
+    }
+
+    if (startingNum > endNum) {
+        let temp = startingNum;
+        startingNum = endNum;
+        endNum = temp;
+    }
+
+    let sum = 0;
+    for (let i = startingNum; i <= endNum; i++) {
+        sum += i;
+    }
+
+    return (Number.isInteger(sum) && sum > 0) ? sum : "ERROR";
 };
 
 // Do not edit below this line
