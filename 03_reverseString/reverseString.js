786b962ced54c6b0762603f870523258fabb85bc
rinnnshinnn <rinnnshinnn@gmail.com>
2024-08-20 12:50:58 +0800

    Add string reversal logic to reverseString function
    - Updated `reverseString` to accept a `string` parameter.
    - Implemented logic to split the input string into an array, reverse the array elements, and join them back into a reversed string.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let array = string.split("");
+    let reversed = [];
+    
+    for (let i = array.length - 1; i >= 0; i--) {
+        reversed.push(array[i]);
+    }
 
+    return reversed.join("");
 };
 
 // Do not edit below this line
